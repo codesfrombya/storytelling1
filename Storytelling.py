7b1a3ee20b758eb361cf24d7a9595a15371772fc
Thamyres Medeiros <thamy.beatriz05@gmail.com>
2026-03-11 23:29:20 -0300

    Add files via upload

---
+++ Storytelling.py
@@ -0,0 +1,17 @@
+import time
+
+def intro():
+    time.sleep(2)
+    print("\n carregando..")
+    time.sleep(4)
+    print("\n Acorde.")
+    time.sleep(3)
+    print("\n Você acorda de um transe em uma floresta escura." )
+    time.sleep(2)
+    print(" Sua cabeça dói.") 
+    time.sleep(2)
+    print("A última coisa de que lembra ter visto, foi um clarão e barulhos altos. De repente, " \
+    "Apareceu nesta floresta com uma dor de cabeça e o que parece ter sido um corte atrás dela.")
+    time.sleep(2)
+    print("Algo mais havia lhe acontecido, você não se lembra de nada. Quem é você? como veio parar aqui? POR QUE EXATAMENTE NO MEIO DA FLORESTA? " \
+    "Quem lhe deu essa pancada na cabeça?")
